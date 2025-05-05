33c209f62336e7a01d8e367a6cc931a7c3748e13
seefeld <seefeld_kevin@yahoo.de>
2025-05-05 16:02:13 +0200

    Added continuous chatting

--- Project/Chatting.py
+++ Project/Chatting.py
@@ -1,14 +1,24 @@
 import Extraction
 from langchain_ollama import ChatOllama
 from langchain.chains import RetrievalQA
+from langchain.memory import ConversationBufferMemory
 
 llm = ChatOllama(model="mistral")
 retriever = Extraction.get_vectorstore().as_retriever()
-qa_chain = RetrievalQA.from_chain_type(llm=llm, retriever=retriever)
+memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+qa_chain = RetrievalQA.from_chain_type(llm=llm, retriever=retriever, memory=memory)
 
-query = "Welche Strategien verwenden Tiere um das Überleben zu sichern?"
-result = qa_chain.invoke(query)
+while True:
+    query = input("\nAsk a question (or type 'exit'): ")
+    if query.lower() == "exit":
+        break
+    result = qa_chain.invoke(query)
+    print("\nAntwort:", result["result"])
 
-print(result["query"])
-print("-------------------------------------------------------------------")
-print(result["result"])
+
+def single_Query(query):
+    llm = ChatOllama(model="mistral")
+    retriever = Extraction.get_vectorstore().as_retriever()
+    qa_chain = RetrievalQA.from_chain_type(llm=llm, retriever=retriever)
+    result = qa_chain.invoke(query)
+    return result["result"]
