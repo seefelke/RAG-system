68e96c07ce553a8fa130e3f651a65e690957e575
seefeld <seefeld_kevin@yahoo.de>
2025-05-15 23:42:06 +0200

    some cleanup of previous code and added second QA chain for testing

--- Project/Chatting.py
+++ Project/Chatting.py
@@ -1,7 +1,10 @@
+from langchain.chains.conversational_retrieval.base import ConversationalRetrievalChain
+from langchain.memory import ConversationBufferWindowMemory
+
 import Extraction
 from langchain_ollama import ChatOllama
 from langchain.chains import RetrievalQA
-from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder, PromptTemplate
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain.chains import create_retrieval_chain
 from langchain.chains import create_history_aware_retriever
@@ -10,28 +13,35 @@ from langchain_core.messages import AIMessage, HumanMessage
 llm = ChatOllama(model="mistral")
 retriever = Extraction.get_vectorstore().as_retriever()
 
-system_prompt_history = (
+# Reformulates the current user question based on chat history if needed to give history context
+# (query, conversation history) -> LLM -> rephrased query -> retriever -> LLM
+history_prompt = (
     "Gegeben sind ein Chatverlauf und die letzte Nutzerfrage,"
     "die sich möglicherweise auf den Kontext im Chatverlauf bezieht."
     "Formuliere eine eigenständige Frage, die auch ohne den Chatverlauf verständlich ist."
     "Beantworte die Frage NICHT, sondern formuliere sie nur um, falls nötig,"
     "und gib sie andernfalls unverändert zurück."
 )
+
+history_template = ChatPromptTemplate(
+    [
+        ("system", history_prompt),
+        MessagesPlaceholder("chat_history"),
+        ("human", "{input}"),
+    ]
+)
+
+# System prompt to answer the actual (reformulated) user query
+
 system_prompt = (
-    "Du bist ein Helfer um Fragen zu beantworten. "
+    "Du bist ein Helfer um Fragen in einem Museum zu beantworten. "
     "Verwende den folgenden zusätzlichen Kontext um deine Antwort zu verbessern."
     "Wenn der Kontext nicht zu der Frage passt und du nicht antworten kannst"
     "dann sag dass du dabei nicht helfen kannst."
     "\n\n"
     "{context}"
 )
-q_prompt = ChatPromptTemplate(
-    [
-        ("system", system_prompt_history),
-        MessagesPlaceholder("chat_history"),
-        ("human", "{input}"),
-    ]
-)
+
 prompt = ChatPromptTemplate.from_messages(
     [
         ("system", system_prompt),
@@ -39,19 +49,52 @@ prompt = ChatPromptTemplate.from_messages(
         ("human", "{input}"),
     ]
 )
+
 history_aware_retriever = create_history_aware_retriever(
-    llm, retriever, q_prompt
+    llm, retriever, history_template
 )
 question_answer_chain = create_stuff_documents_chain(llm, prompt)
 rag_chain = create_retrieval_chain(history_aware_retriever, question_answer_chain)
 
+rag_chain_simple = create_retrieval_chain(retriever, question_answer_chain)
+
 chat_history = []
+
+# Alternative system
+
+prompt_template = PromptTemplate(
+            template=(
+                "Du bist ein deutscher Helfer um Fragen in einem Museum zu beantworten. "
+                "Antworte kurz und präzise in gesprochenem Stil und versuche dich auf maximal 3 Sätze zu beschränken."
+                "Nutze den folgenden Kontext zur Museums Ausstellung um die Fragen zu benatworten.\n\n"
+                "Kontext:\n{context}\n\n"
+                "Verlauf:\n{chat_history}\n\n"
+                "Frage:\n{question}\n\n"
+            ),
+            input_variables=["chat_history", "context", "question"]
+        )
+
+
+memory = ConversationBufferWindowMemory(
+    k=5,  # number of conversation turns (or messages) to keep
+    memory_key="chat_history",
+    return_messages=True
+)
+qa_chain = ConversationalRetrievalChain.from_llm(
+    llm=llm,
+    retriever=retriever,
+    memory=memory,
+    combine_docs_chain_kwargs={"prompt": prompt_template}
+)
+
 
 while True:
     query = input("\nStelle eine Frage (oder 'exit'): ")
     if query.lower() == "exit":
         break
-    result = rag_chain.invoke({"input": query, "chat_history": chat_history})
+    #result = rag_chain.invoke({"input": query, "chat_history": chat_history})
+    result = qa_chain.invoke({"question": query,
+                                   "chat_history": memory.chat_memory.messages})
     chat_history.extend(
         [
             HumanMessage(content=query),
